5e24c88314ba491f078f7d6f87739fe6f154d20a
unknown <chdeming@697-334-Smaug.sf.k12.internal>
2015-09-01 10:33:40 -0700

    have the code to make it work...doesnt work though

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,15 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 int startX = 0;
 int startY = 150;
 int endX = 0;
 int endY = 150;
 
-void setup()
+public void setup()
 {
 	size(300,300);
 	background(0, 0, 255);
 	strokeWeight(10);
 }
-void draw()
+public void draw()
 { 
 	stroke((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 	while(endX <= 300){
@@ -18,7 +34,7 @@ void draw()
 		line(startX, startY, endX, endY);
 	}
 }
-void mousePressed()
+public void mousePressed()
 {
 
 }
@@ -31,3 +47,12 @@ void mousePressed() needs to set startX,startY,endX,endY back to their original 
 You will need to modify index.html to customize your website with it's own title, header and footer. When you are happy with your program, push your finished project up to GitHub
 The final step is to submit the URL for your website via the "drop box" for the assignment on School Loop. Don't hesitate to ask for help if your aren't sure how something is suppose to work.
 */
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
